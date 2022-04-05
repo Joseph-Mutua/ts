a6a4b9b087833bdbd6ab2bb7bcc78a2354621fe3
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-05 13:28:00 +0300

    Aannotations with children

---
+++ rts/src/props/Child.tsx
@@ -0,0 +1,15 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return <div>{Child}
+  <button onClick={onClick}>Click Me</button>
+  </div>;
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
+
