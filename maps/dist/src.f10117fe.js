896cd32e7dc30a90c531b19c290e033108b7f8dd
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-05 09:52:42 +0300

    Add user and company markers

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -25521,7 +25521,7 @@ function () {
     this.name = faker_1.default.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
-      long: parseFloat(faker_1.default.address.longitude())
+      lng: parseFloat(faker_1.default.address.longitude())
     };
   }
 
@@ -25553,7 +25553,7 @@ function () {
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
-      long: parseFloat(faker_1.default.address.longitude())
+      lng: parseFloat(faker_1.default.address.longitude())
     };
   }
 
@@ -25561,7 +25561,52 @@ function () {
 }();
 
 exports.Company = Company;
-},{"@faker-js/faker":"node_modules/@faker-js/faker/dist/esm/index.mjs"}],"src/index.ts":[function(require,module,exports) {
+},{"@faker-js/faker":"node_modules/@faker-js/faker/dist/esm/index.mjs"}],"src/CustomMap.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CustomMap = void 0;
+
+var CustomMap =
+/** @class */
+function () {
+  function CustomMap(divId) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  CustomMap.prototype.addUserMarker = function (user) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    });
+  };
+
+  CustomMap.prototype.addCompanyMarker = function (company) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng
+      }
+    });
+  };
+
+  return CustomMap;
+}();
+
+exports.CustomMap = CustomMap;
+},{}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -25572,11 +25617,16 @@ var User_1 = require("./User");
 
 var Company_1 = require("./Company");
 
+var CustomMap_1 = require("./CustomMap");
+
 var user = new User_1.User();
-var company = new Company_1.Company();
 console.log(user);
+var company = new Company_1.Company();
 console.log(company);
-},{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../.nvm/versions/node/v16.14.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var customMap = new CustomMap_1.CustomMap("map");
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
+},{"./User":"src/User.ts","./Company":"src/Company.ts","./CustomMap":"src/CustomMap.ts"}],"../../../../.nvm/versions/node/v16.14.2/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -25604,7 +25654,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46527" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41219" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
